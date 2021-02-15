12d7ff614b5ecbfe01cd95258821347cbbb840f8
Dan Siegel <me@dansiegel.net>
2021-02-15 09:15:28 -0800

    install and start Appium in the script

--- run-androidtests.sh
+++ run-androidtests.sh
@@ -9,27 +9,38 @@ cd UITest
 
 UITESTPATH=$(pwd)
 
+npm install -g appium
+appium &
+
 echo $UITESTPATH
 
 msbuild ../TestApp.Android/TestApp.Android.csproj /p:Configuration=Release /p:AndroidPackageFormat=apk /p:OutputPath=$UITESTPATH/bin/ /t:SignAndroidPackage
 
 if [ $? -ne 0 ]
 then
-    exit $?
+    exit 1
 fi
 
 msbuild ../TestApp.UITests/TestApp.UITests.csproj /p:OutputPath=$UITESTPATH
 
 if [ $? -ne 0 ]
 then
-    exit $?
+    exit 1
 fi
 
 dotnet run --project=../TestClient/TestClient.csproj
 
 if [ $? -ne 0 ]
 then
-    exit $?
+    exit 1
 fi
 
 dotnet test ../TestApp.UITests/TestApp.UITests.csproj -o=$UITESTPATH --no-build -r=Results --logger trx
+
+ExitCode=$?
+
+AppiumPID=$(ps -A | grep appium | awk '{print $1}')
+echo 'Appium PID: $AppiumPID'
+kill $AppiumPID
+
+exit $ExitCode
--- run-iostests.sh
+++ run-iostests.sh
@@ -11,30 +11,36 @@ UITESTPATH=$(pwd)
 
 echo $UITESTPATH
 
-# npm install -g appium
-# appium &
+npm install -g appium
+appium &
 
 msbuild ../TestApp.iOS/TestApp.iOS.csproj /p:Platform=iPhoneSimulator /p:Configuration=Release /p:OutputPath=$UITESTPATH/bin/
 
 if [ $? -ne 0 ]
 then
-    exit $?
+    exit 1
 fi
 
 msbuild ../TestApp.UITests/TestApp.UITests.csproj /p:OutputPath=$UITESTPATH
 
 if [ $? -ne 0 ]
 then
-    exit $?
+    exit 1
 fi
 
 dotnet run --project=../TestClient/TestClient.csproj
 
 if [ $? -ne 0 ]
 then
-    exit $?
+    exit 1
 fi
 
 dotnet test ../TestApp.UITests/TestApp.UITests.csproj -o=$UITESTPATH --no-build -r=Results --logger trx
 
-# disown
+ExitCode=$?
+
+AppiumPID=$(ps -A | grep appium | awk '{print $1}')
+echo 'Appium PID: $AppiumPID'
+kill $AppiumPID
+
+exit $ExitCode
