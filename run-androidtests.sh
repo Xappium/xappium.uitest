d9c62d5ab7f9af9b0aec789775e0d4d2ecefa2ea
Dan Siegel <me@dansiegel.net>
2021-02-18 14:25:00 -0800

    move appium install to separate build task

--- run-androidtests.sh
+++ run-androidtests.sh
@@ -32,15 +32,8 @@ then
     exit 1
 fi
 
-npm install -g appium
-appium &
-
 dotnet test ../sample/TestApp.UITests/TestApp.UITests.csproj -o=$UITESTPATH --no-build -r=Results --logger trx
 
 ExitCode=$?
-
-AppiumPID=$(ps -A | grep appium | awk '{print $1}')
-echo 'Appium PID: $AppiumPID'
-kill $AppiumPID
 
 exit $ExitCode
