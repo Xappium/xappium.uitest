f0c9bff327526a7f529bec909270f790dad62da1
Dan Siegel <me@dansiegel.net>
2021-03-11 11:42:13 -0800

    add SdkManager tests

--- tests/Xappium.Cli.Tests/Tests/Android/SdkManagerTests.cs
+++ tests/Xappium.Cli.Tests/Tests/Android/SdkManagerTests.cs
@@ -10,6 +10,31 @@ namespace Xappium.Cli.Tests.Android
         public async Task SdkManagerIsInvokable()
         {
             var ex = await Record.ExceptionAsync(() => SdkManager.ExecuteInternal(b => b.Add("--version"), default));
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public async Task InstallsWebDriver()
+        {
+            var ex = await Record.ExceptionAsync(() => SdkManager.InstallWebDriver(default));
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public async Task InstallsLatestCliTools()
+        {
+            var ex = await Record.ExceptionAsync(() => SdkManager.InstallLatestCommandLineTools(default));
+            Assert.Null(ex);
+        }
+
+        [Theory]
+        [InlineData(28)]
+        [InlineData(29)]
+        [InlineData(30)]
+        public async Task InstallAndroidSdk(int sdkLevel)
+        {
+            var ex = await Record.ExceptionAsync(() => SdkManager.EnsureSdkIsInstalled(sdkLevel, default));
+            Assert.Null(ex);
         }
     }
 }
