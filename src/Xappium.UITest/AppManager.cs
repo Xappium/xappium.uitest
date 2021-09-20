2a53c4344a6694644acff79e7a3993af3309353a
Stephen Hawley <sthawl@microsoft.com>
2021-09-20 15:44:45 -0400

    streamline OS check

--- src/Xappium.UITest/AppManager.cs
+++ src/Xappium.UITest/AppManager.cs
@@ -67,29 +67,9 @@ namespace Xappium.UITest
             _engine = null;
         }
 
-        [DllImport("libc")]
-        private static extern int uname(IntPtr buf);
-        private static bool IsRunningOnMacInternal()
+        static bool IsRunningOnMacInternal()
         {
-            IntPtr buf = IntPtr.Zero;
-            try
-            {
-                buf = Marshal.AllocHGlobal(8192);
-                // This is a hacktastic way of getting sysname from uname ()
-                if (uname(buf) == 0)
-                {
-                    string os = Marshal.PtrToStringAnsi(buf);
-                    if (os == "Darwin")
-                        return true;
-                }
-            }
-            catch { }
-            finally
-            {
-                if (buf != IntPtr.Zero)
-                    Marshal.FreeHGlobal(buf);
-            }
-            return false;
+            return RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
         }
     }
 }
